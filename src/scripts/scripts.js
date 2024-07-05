88f00c43a39ba7fc0df0aa15330c0cb3aa7e49b3
Anton Shchukin <a.a.shchukin@gmail.com>
2024-07-05 22:21:32 +0700

    form draft

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -69,4 +69,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+    /* Subscription form */
+
+    (function($) {
+        $('.subscription__submit').on('click', function () {
+            var $this = $(this);
+            if ( ! $this.hasClass('button_loading') &&  ! $this.hasClass('button_success') ) {
+                $this.addClass('button_loading');
+                setTimeout(function () {
+                    $this.removeClass('button_loading').addClass('button_success');
+                    $('.alert').fadeIn(200);
+                }, 3000);
+                setTimeout(function () {
+                    $this.removeClass('button_success');
+                }, 4500);
+            }
+        });
+    })(jQuery);
+
+    (function($) {
+        $('.alert').on('click', function () {
+            $('.alert').fadeOut();
+        });
+
+        $(document).on('keyup', function(event) {
+            if (event.keyCode === 27) { /* Esc */
+                $('.alert').fadeOut();
+            }
+        });
+    })(jQuery);
+
+
 });

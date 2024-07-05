493d5b0467975f6244a0944403b4f23af0f31ef1
Anton Shchukin <a.a.shchukin@gmail.com>
2024-07-05 22:52:33 +0700

    simplify sending

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -77,24 +77,40 @@ document.addEventListener('DOMContentLoaded', () => {
             if ( ! $this.hasClass('button--loading') && ! $this.hasClass('button--success') ) {
                 $this.addClass('button--loading');
 
-                if (Math.random() < 0.5) {
-                    setTimeout(function () {
-                        $this.removeClass('button--loading').addClass('button--success');
+                /* Представим, что 3000ms отправляем данные */
+                setTimeout(function () {
+
+                    /* Как только пришёл ответ убираем button--loading */
+                    $this.removeClass('button--loading');
+
+                    /* И дальше развилка, пусть для примера будует рандом 50/50: */
+                    if (Math.random() < 0.5) {
+
+                        /* Если данные успешно отправлены: */
+                        $this.addClass('button--success');
                         $('.alert--success').fadeIn(200);
-                    }, 3000);
-                    setTimeout(function () {
-                        $this.removeClass('button--success');
-                        $('.alert--success').fadeOut(200);
-                    }, 6000);
-                } else {
-                    setTimeout(function () {
-                        $this.removeClass('button--loading').addClass('button--warning');
+
+                        setTimeout(function () {
+                            $this.removeClass('button--success');
+                            $('.alert--success').fadeOut(200);
+                        }, 3000);
+
+                    } else {
+
+                        /* Если данные не были отправлены: */
+                        $this.addClass('button--warning');
+
                         $('.alert--danger').fadeIn(200);
-                    }, 3000);
-                    setTimeout(function () {
-                        $this.removeClass('button--warning');
-                    }, 6000);
-                }
+                        setTimeout(function () {
+                            $this.removeClass('button--warning');
+                            /* Уведомление в этом случае НЕ убираем -- пусть висит,
+                               пока пользователь не увидит и явно не закроет, или
+                               не начнёт заново заполнять форму */
+                        }, 3000);
+                    }
+
+                }, 3000);
+
             }
         });
     })(jQuery);
